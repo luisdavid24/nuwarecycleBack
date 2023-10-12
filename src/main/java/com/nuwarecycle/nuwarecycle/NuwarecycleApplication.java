1182b9dbb57ff9042a41dc774ca3bb0e2ccba469
Luis David <david12.29@hotmail.com>
2023-10-12 10:29:26 -0500

    Initial commit

---
+++ src/main/java/com/nuwarecycle/nuwarecycle/NuwarecycleApplication.java
@@ -0,0 +1,13 @@
+package com.nuwarecycle.nuwarecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuwarecycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuwarecycleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuwarecycle/nuwarecycle/NuwarecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuwarecycle.nuwarecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuwarecycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
