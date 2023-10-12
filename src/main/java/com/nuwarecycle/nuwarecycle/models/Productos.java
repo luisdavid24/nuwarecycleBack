0e4ac8824508541051ed4c11ef63166a12af4419
Luis David <david12.29@hotmail.com>
2023-10-12 10:59:20 -0500

    ADD productos clase

---
+++ src/main/java/com/nuwarecycle/nuwarecycle/models/Productos.java
@@ -0,0 +1,76 @@
+package com.nuwarecycle.nuwarecycle.models;
+
+import java.util.Date;
+
+public class Productos {
+    private Long id;
+
+    private String nombre;
+
+    private String descripcion;
+
+
+    private float precio;
+
+    private String img;
+
+    private String categoria;
+
+    private int stock;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
