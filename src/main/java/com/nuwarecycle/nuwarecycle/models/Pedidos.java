b2298f75ca616c787fbbf05944f00e28c08bcdc0
JosueMesaP <mesajosue8@gmail.com>
2023-10-12 11:15:09 -0500

    se agrega usuario_id y se cambia nombre de variable a id_pedido

---
+++ src/main/java/com/nuwarecycle/nuwarecycle/models/Pedidos.java
@@ -0,0 +1,19 @@
+package com.nuwarecycle.nuwarecycle.models;
+
+import java.util.Date;
+public class Pedidos {
+
+    private long id_pedido;
+
+    private long usuario_id;
+
+    private Date fecha_creacion;
+
+    private Date fecha_envio;
+
+    private Date fecha_entrega;
+
+    private  String estado;
+
+
+}
