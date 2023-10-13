a8ed0eb2622de5210f2326afe6fe0b8d159ef781
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-10-13 16:16:32 -0500

    Add Mapping Annotations for User Class

--- src/main/java/com/nuwarecycle/nuwarecycle/models/User.java
+++ src/main/java/com/nuwarecycle/nuwarecycle/models/User.java
@@ -1,28 +1,50 @@
 package com.nuwarecycle.nuwarecycle.models;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.Date;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Entity
 @Table(name = "users")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    private int idUser;
+    @Column(name = "username", length = 150, nullable = false)
+    @NonNull
+    private String username;
 
-    private String name;
-
-    private String lastname;
-
-    private String email;
-
+    @Column(name = "password", length = 100, nullable = false)
+    @NonNull
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
-    private String role;
+    @Column(name = "email", length = 100, nullable = false)
+    @NonNull
+    private String email;
 
-    private Date creationDate;
+    @Column(name = "date")
+    @NonNull
+    private LocalDateTime registrationDate;
 
+    @Column(name = "description", length = 400, nullable = false)
+    @NonNull
+    private String description;
 
+    @Column(name = "user_img", length = 100, nullable = false)
+    @NonNull
+    private String profileImg;
 }
