87a48fc878cc6f0abd08cf0b817625c59c7f4008
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-10-13 16:18:43 -0500

    Refactor request mapping endpoint in UserController

--- src/main/java/com/nuwarecycle/nuwarecycle/controllers/UserController.java
+++ src/main/java/com/nuwarecycle/nuwarecycle/controllers/UserController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("api/v1/users")
+@RequestMapping("/api/v1/users")
 public class UserController {
 
     private final UserService userService;
