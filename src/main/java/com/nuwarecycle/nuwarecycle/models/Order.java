ac7cbe072e585738361bacc42c2941cb599c0b99
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-10-13 16:15:35 -0500

    Add Mapping Annotations for Order Class

--- src/main/java/com/nuwarecycle/nuwarecycle/models/Order.java
+++ src/main/java/com/nuwarecycle/nuwarecycle/models/Order.java
@@ -1,25 +1,36 @@
 package com.nuwarecycle.nuwarecycle.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 @Entity
 @Table(name = "orders")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
 
-    private int idOrder;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-    private int user_id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
+    @Column(name = "creationDate", length = 100, nullable = false)
     private Date creationDate;
 
+    @Column(name = "shipment", length = 100, nullable = false)
     private Date shipment;
 
+    @Column(name = "delivery", length = 100, nullable = false)
     private Date delivery;
 
+    @Column(name = "status", length = 50, nullable = false)
     private  String status;
 
 
