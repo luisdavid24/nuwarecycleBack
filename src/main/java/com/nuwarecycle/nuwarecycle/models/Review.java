ef94e8f6fc983c83db80faf4c5ff904684deb17c
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-10-27 21:00:35 -0500

    Create Review Class

---
+++ src/main/java/com/nuwarecycle/nuwarecycle/models/Review.java
@@ -0,0 +1,28 @@
+package com.nuwarecycle.nuwarecycle.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Table(name = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
